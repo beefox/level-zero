824f70994244d8d10826e67c90d9db28fee6b629
beefox <beefox40@outlook.com>
2016-06-06 16:57:19 -0400

    cleaned up comments

--- GaG_LevelZero_Alpha/GaG_LevelZero_Alpha/js/script.js
+++ GaG_LevelZero_Alpha/GaG_LevelZero_Alpha/js/script.js
@@ -76,6 +76,7 @@ $(function () {
             'box-shadow': '2px 2px 5px 2px grey'
         }
         $('#myBGVideo').hide();
+
         // set the game back-ground image setup banner image
         $('#shoot').css({ background: 'url(img/giphy.gif)', 'background-size': 'contain' });
         $('.thumbnail').show();
